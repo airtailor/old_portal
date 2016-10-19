bbfe9011f00b6abfdc22a2b0b9822d79183d27eb
Brian Flynn <bdflynny@gmail.com>
2016-10-19 14:53:42 -0400

    turn off sonar

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -11,15 +11,15 @@ class ItemsController < ApplicationController
     end
 
     # sonar shit
-    # if @order.arrived == true && @order.counter == 0
-    #   SendSonar.message_customer(text: "Hi " + @order.name + ", just a heads up that your Air Tailor order (" + @order.shopify_id + ") has been received! We're going to get to work. In the meantime, stay well :)", to: @customer.phone)
-    #   @order.update_attribute(:counter, 1)
-    # end
+    if @order.arrived == true && @order.counter == 0
+      # SendSonar.message_customer(text: "Hi " + @order.name + ", just a heads up that your Air Tailor order (" + @order.shopify_id + ") has been received! We're going to get to work. In the meantime, stay well :)", to: @customer.phone)
+      @order.update_attribute(:counter, 1)
+    end
 
-    # if @order.complete == true && @order.counter == 1
-    #   SendSonar.message_customer(text: "Good news " + @customer.first_name + " — your Air Tailor order is finished and on its way to you!", to: @customer.phone )
-    #   @order.update_attribute(:counter, 2)
-    # end
+    if @order.complete == true && @order.counter == 1
+      # SendSonar.message_customer(text: "Good news " + @customer.first_name + " — your Air Tailor order is finished and on its way to you!", to: @customer.phone )
+      @order.update_attribute(:counter, 2)
+    end
     # end sonar shit
 
     # begin customer measurement section
