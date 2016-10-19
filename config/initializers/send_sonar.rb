9e3b9e438fae9d16ff5c72563c1028a55d6e9d07
Brian Flynn <bdflynny@gmail.com>
2016-10-19 13:26:54 -0400

    change small stuff for Heroku

--- config/initializers/send_sonar.rb
+++ config/initializers/send_sonar.rb
@@ -3,8 +3,9 @@
 SendSonar.configure do |config|
   if Rails.env.production?
     config.env = :live
-    config.token = ENV['SONAR_PRODUCTION_TOKEN']
+    config.token = ENV['SONAR_SANDBOX_TOKEN']
     # || 'YOUR_PRODUCTION_TOKEN'
+    # 'SONAR_PRODUCTION_TOKEN'
 
   elsif Rails.env.development?
     config.env = :sandbox
