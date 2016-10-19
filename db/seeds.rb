8e3b9c7105b9667aeb0be1e0aee2d73f73c53b55
Brian Flynn <bdflynny@gmail.com>
2016-10-19 13:24:59 -0400

    change small stuff for Heroku

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,5 @@
-blah = User.create(email: "brian@faketailor.com", password: "12345", business_name: "Fake Tailor", user_name: "Brian", phone: "616-456-7890", street: "123 Fake St.", unit: "1", city: "Grand Rapids", state: "MI", country: "USA", timezone: "eastern")
-User.create(email: "brian@airtailor.com", password: "12345", business_name: "Air Tailor", user_name: "Brian", phone: "616-780-4457", street: "123 Awesome St.", unit: "53", city: "Grand Rapids", state: "MI", country: "USA", timezone: "eastern")
-User.create(email: "joshua@imaginarytailor.com", password: "12345", business_name: "Imaginary Tailor", user_name: "Joshua", phone: "347-456-7890", street: "123 Imaginary St.", unit: "2", city: "Los Angeles", state: "CA", country: "USA", timezone: "pacific")
-User.create(email: "vlad@mythicaltailor.com", password: "12345", business_name: "Mythical Tailor", user_name: "Vlad", phone: "313-456-7890", street: "123 Myth Way", unit: "3", city: "Austin", state: "TX", country: "USA", timezone: "central")
+User.create(email: "brian@airtailor.com", password: "12345", business_name: "Air Tailor", user_name: "Brian", phone: "616-780-4457", street: "510 West 21st Street", unit: "65DM8A", city: "New York", state: "NY", zip: "10011", country: "US", timezone: "eastern")
+
 
 # Order.create(name: "customer 1",
 #   shopify_id: 1234,
@@ -15,12 +13,12 @@ User.create(email: "vlad@mythicaltailor.com", password: "12345", business_name: 
 #   total: "20.00",
 #   alterations: ["Dress #1 Add Belt Buttons"])
 
-Conversation.create(sender_id: 2, recipient_id: 1)
+# Conversation.create(sender_id: 2, recipient_id: 1)
 
-Message.create(user_id: 2, conversation_id: 1, text: "Testing message system", read: true, user_read: false)
-Message.create(user_id: 1, conversation_id: 1, text: "Hey! It works.", read: false, user_read: true)
-Message.create(user_id: 2, conversation_id: 1, text: "Oh that's great to hear!", read: true, user_read: false)
-Message.create(user_id: 1, conversation_id: 1, text: "Yeah, you're really awesome at this. Don't give up!", read: false, user_read:true)
+# Message.create(user_id: 2, conversation_id: 1, text: "Testing message system", read: true, user_read: false)
+# Message.create(user_id: 1, conversation_id: 1, text: "Hey! It works.", read: false, user_read: true)
+# Message.create(user_id: 2, conversation_id: 1, text: "Oh that's great to hear!", read: true, user_read: false)
+# Message.create(user_id: 1, conversation_id: 1, text: "Yeah, you're really awesome at this. Don't give up!", read: false, user_read:true)
 
 # Order.create(name: "Daffy Duck",
 #   shopify_id: 3456234,
