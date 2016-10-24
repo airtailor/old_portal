870e6adf62dcf5ff561311d2635672921db38306
Brian Flynn <bdflynny@gmail.com>
2016-10-24 16:54:08 -0400

    add shipping label to order params

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -64,7 +64,7 @@ class OrdersController < ApplicationController
   private
 
   def order_params
-    params.require(:order).permit(:name, :shopify_id, :unique_id, :total, :alterations, :user_id, :arrival_date, :due_date, :complete, :arrived, :customer_id, :counter)
+    params.require(:order).permit(:name, :shopify_id, :unique_id, :total, :alterations, :user_id, :arrival_date, :due_date, :complete, :arrived, :customer_id, :counter, :shipping_label)
   end
 
 end
