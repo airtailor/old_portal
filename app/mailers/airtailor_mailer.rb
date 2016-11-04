c64febe07da84bc2177260033b7a408652288d5c
Brian Flynn <bdflynny@gmail.com>
2016-11-04 14:07:55 -0400

    turn off live features

--- app/mailers/airtailor_mailer.rb
+++ app/mailers/airtailor_mailer.rb
@@ -8,10 +8,10 @@ class AirtailorMailer < ApplicationMailer
     @customer = customer
     @order = order
     # html=render_to_string(:partial=> "attachment")
-    kit = PDFKit.new "<img src=" + @order.inbound_label.to_s + " style='display:block;text-align:center;width:60%; padding-bottom:20px; border-bottom:1px dashed black'><h2 style='text-align:center; padding-top:100px; font-size:48px'>" + @order.shopify_id.to_s + "</h2>"
-    kit.to_file("#{Rails.root}/public/kit.pdf")
+    label = PDFKit.new "<img src=" + @order.inbound_label.to_s + " style='display:block;text-align:center;width:60%; padding-bottom:20px; border-bottom:1px dashed black'><h2 style='text-align:center; padding-top:100px; font-size:48px'>" + @order.shopify_id.to_s + "</h2>"
+    label.to_file("#{Rails.root}/public/label.pdf")
 
-    attachments["kit.pdf"] = File.read(Rails.root.join('public',"kit.pdf"))
-    mail(to: @customer.email.to_s, subject: 'Sample Portal Email')
+    attachments["label.pdf"] = File.read(Rails.root.join('public',"label.pdf"))
+    mail(to: "bdflynny@gmail.com", subject: 'Air Tailor Shipping Label')
   end
 end
