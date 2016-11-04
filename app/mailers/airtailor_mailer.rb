9b37f490fca392046824c93f77e91bc765cfb820
Brian Flynn <bdflynny@gmail.com>
2016-11-03 21:32:44 -0400

    add pdf attachment to customer email

--- app/mailers/airtailor_mailer.rb
+++ app/mailers/airtailor_mailer.rb
@@ -7,7 +7,7 @@ class AirtailorMailer < ApplicationMailer
 
     @customer = customer
     @order = order
-    html=render_to_string(:partial=> "attachment")
+    # html=render_to_string(:partial=> "attachment")
     kit = PDFKit.new "<img src=" + @order.inbound_label.to_s + " style='width:40%;padding-bottom:20px;border-bottom:1px dashed black'>"
     kit.to_file("#{Rails.root}/public/kit.pdf")
 
