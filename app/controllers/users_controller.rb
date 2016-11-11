bb889b617b3301985be74879d117945957d29111
Brian Flynn <bdflynny@gmail.com>
2016-11-11 17:47:50 -0500

    add tracking number to texts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -35,7 +35,7 @@ class UsersController < ApplicationController
 
       if @owner && @order.welcome == false && @order.inbound_counter != 1
           tailorShippingInfo(@owner, @order, @customer)
-        if @order.inbound_label?
+        if @order.inbound_label != nil
           AirtailorMailer.label_email(@customer, @order).deliver
           flash[:inbound] = "Inbound Label Sent!"
           @order.update_attribute(:inbound_counter, 1)
