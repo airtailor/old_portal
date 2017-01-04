154edbf8a16e17187e38b1f1f187ca7413e60175
Brian Flynn <bdflynny@gmail.com>
2017-01-04 12:36:54 -0500

    fix debra berman order

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -11,10 +11,6 @@ class ItemsController < ApplicationController
       @shipment = Shipment.where(shopify_id: @order.shopify_id)
     end
 
-    if @order.shopify_id == "#3695"
-      @order.update_attribute(:counter, 2)
-    end
-
     fullname = @customer.first_name.capitalize + " " + @customer.last_name.capitalize
 
     @order.update_attribute(:name, fullname)
