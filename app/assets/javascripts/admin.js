2fec17f4a2104783c47e5ffb40736500a4d81e23
Brian Flynn <bdflynny@gmail.com>
2017-01-16 20:09:20 -0500

    add stats for current week to dashboard

--- app/assets/javascripts/admin.js
+++ app/assets/javascripts/admin.js
@@ -1,13 +1,20 @@
+$(document).on("click", "#this-week-button", function(){
+      $("#this-week").show()
+      $("#this-month").hide()
+      $("#last-month").hide()
 
+  });
 
 $(document).on("click", "#this-month-button", function(){
       $("#this-month").show()
       $("#last-month").hide()
+      $("#this-week").hide()
   });
 
 $(document).on("click", "#last-month-button", function(){
-      $("#this-month").hide()
       $("#last-month").show()
+      $("#this-month").hide()
+      $("#this-week").hide()
   });
 
 $(document).on("click", "#all-button", function(){
