537f92e90aae5e4bcf4504dfae7370ba58e33056
Brian Flynn <bdflynny@gmail.com>
2017-01-24 10:44:23 -0500

    update dashboard format

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -22,7 +22,7 @@ class UsersController < ApplicationController
       @week_end = Date.today.end_of_week
 
       @last_week_begin = @week_begin - 7.days
-      @last_week_end = @week_end - 8.days
+      @last_week_end = @week_end - 6.days
 
       @this_week = []
       @this_week_rev = 0
@@ -45,7 +45,7 @@ class UsersController < ApplicationController
         if kit.created_at >= @week_begin && kit.created_at <= @week_end
           @this_week_kits.push(kit)
         end
-        if kit.created_at >= @last_week_begin && kit.created_at < @last_week_end
+        if kit.created_at > @last_week_begin && kit.created_at <= @last_week_end
           @last_week_kits.push(kit)
         end
         if kit.created_at.strftime("%Y%m") == @current_month
