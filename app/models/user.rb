0eb3446c32043a67a2480764fa645c45cc0f73c9
Brian Flynn <bdflynny@gmail.com>
2017-04-13 23:58:01 -0400

    turn off pw validation to update Hulan

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ActiveRecord::Base
   validates :email, presence: true, uniqueness: true
   validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
 
-  # validates :password, presence: true, length: { minimum: 5 }
+  validates :password, presence: true, length: { minimum: 5 }
 
   def is_admin?
     # @TODO Make this better.
